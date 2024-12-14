ff4df8aaac2baeae26024c2d5b38e4bdb2be9623
ivanfebrianto <specopskit@gmail.com>
2024-12-14 10:37:21 +0700

    feat:Add Guessing Game

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+extern  crate rand;
+use core::num;
+use std::{cmp::Ordering, io};
+use rand::Rng;
+
+fn main() {
+    println!("Guess The Number!");
+    let secret_number = rand::thread_rng().gen_range(1..10);
+    println!("The Secret Number is : {}",secret_number);  //You can hide this message
+    loop {
+        
+        println!("Please Input a Number: ");
+        let mut guess = String::new(); 
+        io::stdin().read_line(&mut guess)
+        .expect("Failed to read line!!");
+        let guess : i32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You Guessed: {}",guess);
+        match guess.cmp(&secret_number) {
+            Ordering ::Less => println!("Too Small!"),
+            Ordering ::Greater => println!("Too Big!"),
+            Ordering ::Equal => {
+                println!("You Win!");
+                break;
+            }   
+        }
+}
+}
